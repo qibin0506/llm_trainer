16ad227a4c277a3cc9d59bb3af12a0c939a11d87
qibin <qibin0506@users.noreply.github.com>
2024-12-30 08:16:00 +0000

    support Knowledge Distillation

--- llm_trainer/__init__.py
+++ llm_trainer/__init__.py
@@ -1,7 +1,7 @@
 from .trainer import train
 from .train_tools import TrainerTools
 from .generate_utils import generate
-from .train_args import TrainArgs, FsdpArgs, DataLoaderArgs
+from .train_args import TrainArgs, FsdpArgs, DataLoaderArgs, LrSchedulerArgs, KDArgs
 from .checkpoint import load_checkpoint
 from .dcp import load_dcp, convert_dcp_to_pth
 

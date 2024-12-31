2e8a4c6cb33865bbc3d1fa3c0f20e053ecd6392b
qibin <qibin0506@users.noreply.github.com>
2024-12-31 19:31:03 +0800

    Update checkpoint.py

--- llm_trainer/checkpoint.py
+++ llm_trainer/checkpoint.py
@@ -61,21 +61,22 @@ def load_checkpoint(
         device: Optional[Union[torch.device, str]] = None
 ):
     checkpoint_name = os.environ.get('CHECKPOINT_NAME', DEFAULT_CHECKPOINT_NAME)
-
     if os.environ.get('ENABLE_DCP', '1') == '1':
         if isinstance(model, FSDP):
             load_dcp(model, optimizer)
         else:
             # load_dcp方式在cpu上会报错，所以改为先将ckpt转换为pth，然后再加载pth
             # load_dcp(model, optimizer)
-            if os.path.exists(checkpoint_name):
-                os.remove(checkpoint_name)
-
+            checkpoint_name = f'{checkpoint_name}_temp.pth'
             convert_dcp_to_pth(checkpoint_name)
             ckpt = torch.load(checkpoint_name, map_location=device, weights_only=True)
             model.load_state_dict(ckpt['app']['model_state_dict'])
             if optimizer is not None:
                 optimizer.load_state_dict(ckpt['app']['optim_state_dict'])
+
+            # 使用完删除
+            if os.path.exists(checkpoint_name):
+                os.remove(checkpoint_name)
     else:
         if os.path.exists(checkpoint_name):
             # 未经过测试，else的逻辑经过测试在fsdp下也没问题

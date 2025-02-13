b1752a9d46df69928cbf743f6beef369cf6a76df
qibin <qibin0506@users.noreply.github.com>
2025-02-13 07:02:56 +0000

    sft support multi-turn conversation

--- llm_trainer/utils.py
+++ llm_trainer/utils.py
@@ -23,22 +23,40 @@ def pretrain_collate_fn(batch_data):
 def sft_collate_fn(batch_data):
     """
      如果是sft，则不计算prompt部分的loss, 例如：
-    logits: [USER]:你好[BOT]:我好[SEP]
-    labels: [USER]:你好[BOT]:我好[SEP]
+    logits: [USER]你好[BOT]我好[SEP]
+    labels: [USER]你好[BOT]我好[SEP]
 
-    shift_logits: [USER]:你好[BOT]:我好
-    shift_labels: :你好[BOT]:我好[SEP]
+    shift_logits: [USER]你好[BOT]我好
+    shift_labels: 你好[BOT]我好[SEP]
 
-    mask_labels: mask mask mask mask:我好[SEP]
-        * [BOT]后的:暂时不考虑，后续可以把prompt里的：去掉
+    mask_labels: mask mask mask mask 我好[SEP]
         * mask=-100和pad一样
-    """
-    inputs, labels = pretrain_collate_fn(batch_data)
-    batch_size = len(labels)
-    batch_bot_idx = torch.nonzero(torch.eq(labels, TrainerTools().tokenizer.bot), as_tuple=True)[1]
 
-    for batch in range(batch_size):
-        bot_idx = batch_bot_idx[batch].item() + 1
-        labels[batch, :bot_idx] = torch.tensor([-100] * bot_idx)
+
+    多轮对话场景
+    [USER]你好[BOT]我好[SEP][USER]很好[BOT]不好[SEP]
+    mask: mask mask mask mask 我好[SEP] mask mask mask mask 不好[SEP]
+    """
+
+    inputs, labels = pretrain_collate_fn(batch_data)
+
+    # 支持多轮会话的mask
+    for batch, label in enumerate(labels):
+        start_index = -1
+        for index, token in enumerate(label):
+            if token == TrainerTools().tokenizer.user:
+                start_index = index
+            elif token == TrainerTools().tokenizer.bot and start_index != -1:
+                labels[batch, start_index:index + 1] = -100
+                start_index = -1
+
+    # 支持单轮会话的mask
+    # inputs, labels = pretrain_collate_fn(batch_data)
+    # batch_size = len(labels)
+    # batch_bot_idx = torch.nonzero(torch.eq(labels, TrainerTools().tokenizer.bot), as_tuple=True)[1]
+    #
+    # for batch in range(batch_size):
+    #     bot_idx = batch_bot_idx[batch].item() + 1
+    #     labels[batch, :bot_idx] = torch.tensor([-100] * bot_idx)
 
     return inputs, labels

e9d8632de2e084328eff4ab99c846ac7cc4ad77a
qibin <qibin0506@users.noreply.github.com>
2025-02-17 10:41:29 +0000

    add Yi tokenizer, remove bert tokenizer

--- llm_trainer/tokenizer.py
+++ llm_trainer/tokenizer.py
@@ -1,33 +1,35 @@
 import os
-from transformers import BertTokenizerFast
 from transformers import Qwen2TokenizerFast
+from transformers import AutoTokenizer
 from transformers import AddedToken
 import torch
 
-TOKEN_TYPE_BERT = 'bert'
 TOKEN_TYPE_QWEN = 'qwen'
+TOKEN_TYPE_YI = "yi"
 
-AVAILABLE_TOKEN_TYPES = [TOKEN_TYPE_BERT, TOKEN_TYPE_QWEN]
+AVAILABLE_TOKEN_TYPES = [TOKEN_TYPE_QWEN, TOKEN_TYPE_YI]
 
 
 class Tokenizer:
-    def __init__(self, token_type: str = TOKEN_TYPE_QWEN):
+    def __init__(self, token_type: str = TOKEN_TYPE_YI):
         super().__init__()
 
         assert token_type in AVAILABLE_TOKEN_TYPES, 'token type is unavailable'
 
-        self.eot_text = '[SEP]'
-        self.pad_text = '[PAD]'
-        self.unk_text = '[UNK]'
-        self.user_text = '[USER]'
-        self.bot_text = '[BOT]'
+        self.eot_text = '<|endoftext|>'
 
-        if token_type == TOKEN_TYPE_BERT:
-            self.tokenizer = BertTokenizerFast(f"{os.environ['TOKEN_DIR']}bert_vocab.txt")
-            self.eot = self.tokenizer.sep_token_id
-            self.pad = self.tokenizer.pad_token_id
-            self.unk = self.tokenizer.unk_token_id
-        elif token_type == TOKEN_TYPE_QWEN:
+        self.pad_text = '<|pad|>'
+        self.unk_text = '<unk>'
+
+        self.user_text = '<|user|>'
+        self.bot_text = '<|assistant|>'
+
+        self.bor_text = '<|beginofreasoning|>'
+        self.eor_text = '<|endofreasoning|>'
+
+        additional_special_tokens = []
+
+        if token_type == TOKEN_TYPE_QWEN:
             self.tokenizer = Qwen2TokenizerFast(
                 vocab_file=f"{os.environ['TOKEN_DIR']}qwen_vocab.json",
                 merges_file=f"{os.environ['TOKEN_DIR']}qwen_merges.txt",
@@ -35,16 +37,27 @@ class Tokenizer:
                 eos_token=self.eot_text,
                 pad_token=self.pad_text
             )
-            self.eot = self.tokenizer.eos_token_id
-            self.pad = self.tokenizer.pad_token_id
-            self.unk = self.tokenizer.unk_token_id
+        else:
+            self.tokenizer = AutoTokenizer.from_pretrained(os.environ['TOKEN_DIR'])
+            additional_special_tokens.append(AddedToken(self.pad_text, lstrip=False, rstrip=False))
 
-        added_user_token = AddedToken(self.user_text, lstrip=False, rstrip=False)
-        added_bot_token = AddedToken(self.bot_text, lstrip=False, rstrip=False)
-        self.tokenizer.add_special_tokens({"additional_special_tokens": [added_user_token, added_bot_token]})
+        additional_special_tokens.append(AddedToken(self.user_text, lstrip=False, rstrip=False))
+        additional_special_tokens.append(AddedToken(self.bot_text, lstrip=False, rstrip=False))
+        additional_special_tokens.append(AddedToken(self.bor_text, lstrip=False, rstrip=False))
+        additional_special_tokens.append(AddedToken(self.eor_text, lstrip=False, rstrip=False))
+
+        self.tokenizer.add_special_tokens({"additional_special_tokens": additional_special_tokens})
+
+        self.eot = self.tokenizer.convert_tokens_to_ids(self.eot_text)
+
+        self.pad = self.tokenizer.convert_tokens_to_ids(self.pad_text)
+        self.unk = self.tokenizer.convert_tokens_to_ids(self.unk_text)
 
         self.user = self.tokenizer.convert_tokens_to_ids(self.user_text)
         self.bot = self.tokenizer.convert_tokens_to_ids(self.bot_text)
+
+        self.bor = self.tokenizer.convert_tokens_to_ids(self.bor_text)
+        self.eor = self.tokenizer.convert_tokens_to_ids(self.eor_text)
 
         self.vocab_size = len(self.tokenizer)
 
@@ -66,23 +79,15 @@ class Tokenizer:
         return self.tokenizer.decode(token.squeeze(0))
 
 
-# if __name__ == '__main__':
-#     tokenizer = Tokenizer(TOKEN_TYPE_QWEN)
-#     print(tokenizer.vocab_size)
-#
-#     eot = tokenizer.eot
-#     pad = tokenizer.pad
-#     unk = tokenizer.unk
-#
-#     print(eot, pad, unk)
-#     print(tokenizer.decode_to_text(torch.tensor([eot])),
-#           tokenizer.decode_to_text(torch.tensor([pad])),
-#           tokenizer.decode_to_text(torch.tensor([unk])))
-#
-#     print(tokenizer.encode_to_token(' '))
-#     print(tokenizer.encode_to_token('[USER]'))
-#     print(tokenizer.encode_to_token('[BOT]'))
-#     print(tokenizer.user)
-#     print(tokenizer.bot)
+if __name__ == '__main__':
+    tokenizer = Tokenizer(TOKEN_TYPE_YI)
+    print(tokenizer.vocab_size)
+    print(tokenizer.encode_to_token(tokenizer.eot_text), tokenizer.eot)
+    print(tokenizer.encode_to_token(tokenizer.pad_text), tokenizer.pad)
+    print(tokenizer.encode_to_token(tokenizer.unk_text), tokenizer.unk)
+    print(tokenizer.encode_to_token(tokenizer.user_text), tokenizer.user)
+    print(tokenizer.encode_to_token(tokenizer.bot_text), tokenizer.bot)
+    print(tokenizer.encode_to_token(tokenizer.bor_text), tokenizer.bor)
+    print(tokenizer.encode_to_token(tokenizer.eor_text), tokenizer.eor)
 
 

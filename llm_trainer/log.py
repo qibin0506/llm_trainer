8568fbfde91d9e40666ab2cd6b83c25d607d5d32
qibin <qibin0506@users.noreply.github.com>
2025-05-19 20:44:16 +0800

    Update log.py

--- llm_trainer/log.py
+++ llm_trainer/log.py
@@ -10,7 +10,7 @@ def get_log_dir() -> str:
 def log(msg: str, log_file=None):
     cur_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     if not log_file:
-        print(f'({cur_time}) {msg}')
+        print(f'[{cur_time}] {msg}')
     else:
         with open(log_file, 'a') as f:
-            f.write(f"({cur_time}) {msg}")
+            f.write(f"[{cur_time}] {msg}")

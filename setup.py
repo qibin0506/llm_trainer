57e90b7bdc745c9cc7c85f1cd4e7674570820e59
Bin Qi <qibin0506@users.noreply.github.com>
2025-12-02 13:21:46 +0000

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='project_llm_trainer',
-    version='0.11.1',
+    version='0.11.2',
     description='LLM and VLM trainer',
     scripts=[
         'scripts/smart_train',

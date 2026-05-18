b44898f81cc1533ad358c43d5a6308fc08a5d9c9
Binn Chi <qibin0506@gmail.com>
2026-05-18 19:33:49 +0800

    Release 0.18.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='project_llm_trainer',
-    version='0.17.3',
+    version='0.18.0',
     description='LLM and VLM trainer',
     scripts=[
         'scripts/smart_train',

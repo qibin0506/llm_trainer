f7d66015c10a5dd63b401a718e2c2cc53ec65095
Binn Chi <qibin0506@gmail.com>
2026-05-22 17:24:39 +0800

    Bump version from 0.18.4 to 0.19.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='project_llm_trainer',
-    version='0.18.4',
+    version='0.19.0',
     description='LLM and VLM trainer',
     scripts=[
         'scripts/smart_train',
